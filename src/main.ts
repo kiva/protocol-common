811d39000ffeca839f1806b18b4689c5bba08a17
Matt Raffel <mattr@kiva.org>
2020-09-02 09:50:53 -0600

    moves delay function into a class
    Signed-off-by: Matt Raffel <mattr@kiva.org>

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,4 @@ export function version(): string {
     return '1.0';
 }
 
-/*
-    Protocol-common version of a sleep function
-    input is milliseconds
-*/
-export async function delay(ms: number): Promise<any> {
-    return new Promise( resolve => setTimeout(resolve, ms) );
-}
+
