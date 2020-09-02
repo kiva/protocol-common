9e27241bbc6253df910283dccd6cbdd22dbe8707
Matt Raffel <mattr@kiva.org>
2020-09-02 09:33:52 -0600

    implementing a sleep function.  keeping consistent with protocol and aries implementations, calling it delay
    Signed-off-by: Matt Raffel <mattr@kiva.org>

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,11 @@
 export function version(): string {
     return '1.0';
 }
+
+/*
+    Protocol-common version of a sleep function
+    input is milliseconds
+*/
+export async function delay(ms: number): Promise<any> {
+    return new Promise( resolve => setTimeout(resolve, ms) );
+}
