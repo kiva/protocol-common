1bdd805e6db1779794411899748ebb2c1507aeb8
Jeff Kennedy <69258574+jeffk-kiva@users.noreply.github.com>
2021-01-27 09:13:41 -0800

    PRO-2696: Update error codes (#26)
    * Fix the missing underscore in the FINGERPRINT_NOMATCH error code.
    * Add bio auth error codes.
    * Add sms error codes.
    
    Signed-off-by: Jeff Kennedy <jeffk@kiva.org>

--- src/protocol.errorcode.ts
+++ src/protocol.errorcode.ts
@@ -2,8 +2,9 @@
  * Different error codes for protocol related exceptions.
  */
 export enum ProtocolErrorCode {
+
+    // Common error codes
     COMMON_ERROR = 'CommonError',
-    FINGERPRINT_NOMATCH = 'FingerprintNoMatch',
     MISSING_DEVICE_DETAILS = 'MissingDeviceDetails',
     INTERNAL_SERVER_ERROR = 'InternalServerError',
     INVALID_NODE_ENVIRONMENT = 'InvalidNodeEnvironment',
@@ -16,5 +17,30 @@ export enum ProtocolErrorCode {
     INVALID_TOKEN = 'InvalidToken',
     MISSING_AGENT_ID = 'MissingAgentId',
     MISSING_CONFIGURATION = 'MissingConfiguration',
-    MISSING_SPAN = 'MissingSpan'
+    MISSING_SPAN = 'MissingSpan',
+
+    // Bio Auth error codes
+    BIOANALYZER_SERVER_ERROR = 'BioanalyzerServerError',
+    FINGERPRINT_NO_MATCH = 'FingerprintNoMatch',
+    FINGERPRINT_LOW_QUALITY = 'FingerprintLowQuality',
+    FINGERPRINT_MISSING_NOT_CAPTURED = 'FingerprintMissingNotCaptured',
+    FINGERPRINT_MISSING_AMPUTATION = 'FingerprintMissingAmputation',
+    FINGERPRINT_MISSING_UNABLE_TO_PRINT = 'FingerprintMissingUnableToPrint',
+    INVALID_FILTERS = 'InvalidFilters',
+    INVALID_PARAMS = 'InvalidParams',
+    INVALID_IMAGE_ENCODING = 'InvalidImageEncoding',
+    INVALID_IMAGE_FORMAT = 'InvalidImageFormat',
+    INVALID_POSITION = 'InvalidPosition',
+    INVALID_DATA_TYPE = 'InvalidDataType',
+    INVALID_TEMPLATE_VERSION = 'InvalidTemplateVersion',
+    INVALID_BACKEND_NAME = 'InvalidBackendName',
+    INVALID_BACKEND_OPERATION = 'InvalidBackendOperation',
+
+    // SMS error codes
+    NO_PHONE_NUMBER = 'NoPhoneNumber',
+    PHONE_NUMBER_NO_MATCH = 'PhoneNumberNoMatch',
+    SMS_SEND_FAILED = 'SmsSendFailed',
+    OTP_NO_MATCH = 'OtpNoMatch',
+    OTP_EXPIRED = 'OtpExpired',
+    TOO_MANY_ATTEMPTS = 'TooManyAttempts'
 }
