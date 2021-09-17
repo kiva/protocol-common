51a616c08b8f0f0f15f01c6cf68165ba5a819937
Jeff Kennedy <69258574+jeffk-kiva@users.noreply.github.com>
2021-09-17 09:22:24 -0700

    Deploy only if the version has been updated (#51)
    * Deploy only if the version has been updated
    
    Signed-off-by: Jeff Kennedy <jeffk@kiva.org>

--- publish.sh
+++ publish.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 set -ex
 
-cp package.json dist/
-cp README.md dist/
-cp .npmrc dist/
-cd dist
-npm publish --verbose
-cd ..
+localVersion=$(jq -r '.version' package.json)
+remoteVersion=$(npm show protocol-common version)
+
+if [ "$localVersion" == "$remoteVersion" ]; then
+  echo "The version of protocol-common has not been updated in package.json. Skipping publish step."
+else
+  echo "Publishing $localVersion"
+  cp package.json dist/
+  cp README.md dist/
+  cp .npmrc dist/
+  cd dist
+  npm publish --verbose
+  cd ..
+fi
