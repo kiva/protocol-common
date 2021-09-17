6930afe25ccb8a0ef5f6d8b479e2b96c839bcd79
Jeff Kennedy <jeffk@kiva.org>
2021-09-17 09:07:12 -0700

    Undo version update. Run the publish script only on main branch.
    Signed-off-by: Jeff Kennedy <jeffk@kiva.org>

--- publish.sh
+++ publish.sh
@@ -7,11 +7,11 @@ remoteVersion=$(npm show protocol-common version)
 if [ "$localVersion" == "$remoteVersion" ]; then
   echo "The version of protocol-common has not been updated in package.json. Skipping publish step."
 else
-#  cp package.json dist/
-#  cp README.md dist/
-#  cp .npmrc dist/
-#  cd dist
-#  npm publish --verbose
-#  cd ..
   echo "Publishing $localVersion"
+  cp package.json dist/
+  cp README.md dist/
+  cp .npmrc dist/
+  cd dist
+  npm publish --verbose
+  cd ..
 fi
